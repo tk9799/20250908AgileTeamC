f749c5163cf05d8fcb7258273fe5efc47d9956c0
game\242j005 <ttno6018@gmail.com>
2025-10-09 11:25:22 +0900

    ナイフの回収、投げるナイフの見た目をキューブからナイフに変更しました。

--- My project/Assets/Takahashi/Script/KnifeController.cs
+++ My project/Assets/Takahashi/Script/KnifeController.cs
@@ -3,16 +3,22 @@ using UnityEngine;
 public class KnifeControllertr : MonoBehaviour
 {
     public int damage = 10;
-    public GameObject owner; // �N�������������L�^
+    public GameObject owner; // 誰が撃ったかを記録
+    Rigidbody rb;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
 
     private void Start()
     {
-        //Destroy(gameObject, lifeTime); // ��莞�Ԍ�ɒe������
+        //Destroy(gameObject, lifeTime); // 一定時間後に弾を消す
     }
 
     private void OnTriggerEnter(Collider other)
     {
-        // �������������v���C���[�ɓ��������疳������ꍇ�͂����Ń`�F�b�N
+        // 自分を撃ったプレイヤーに当たったら無視する場合はここでチェック
         if (other.gameObject == owner)
         {
             return;
@@ -22,7 +28,29 @@ public class KnifeControllertr : MonoBehaviour
         if (health != null)
         {
             health.TakeDamage(damage);
-            Destroy(gameObject); // �e������
+            Destroy(gameObject); // 弾を消す
+        }
+
+        if (other.gameObject.CompareTag("Wall"))
+        {
+            
+            if (rb != null)
+            {
+                rb.isKinematic = true; // 物理挙動を止める
+                //transform.position = Vector3.zero;
+                //rb.linearVelocity = Vector3.zero;
+                //rb.angularVelocity = Vector3.zero;
+                Debug.Log(rb.isKinematic);
+            }
+            this.gameObject.tag = "NotPossessionKnife";//誰のものでもないナイフにする
+            transform.parent = other.transform;//壁にくっつける
+
+            
+        }
+
+        if (other.gameObject.CompareTag("Player")&&this.gameObject.tag== "NotPossessionKnife")
+        {
+            Debug.Log("ナイフを回収");
         }
     }
 }
