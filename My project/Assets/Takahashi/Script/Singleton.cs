a8d7c6e78539411a9b63f9365b5e842ba170fbaf
GAME\242j011 <abeharu0221@gmail.com>
2025-11-28 12:06:32 +0900

    Singletonにシーン関連の変数をまとめました。

--- My project/Assets/Takahashi/Script/Singleton.cs
+++ My project/Assets/Takahashi/Script/Singleton.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Singleton : MonoBehaviour
 {
@@ -6,9 +7,52 @@ public class Singleton : MonoBehaviour
 
     public string winnerTeamName = "";
 
-    public string GetWinnerName()
+    // シーン遷移用の変数
+    // ここではタイトルシーンを入力する
+    public string titleScene = "";
+
+    // シーン遷移用の変数
+    // キャラクター選択シーンを入力する
+    public string choseCharactorScene = "";
+
+    // シーン遷移用の変数
+    // チュートリアルシーンを入力する
+    public string tutorialScene = "";
+
+    // シーン遷移用の変数
+    // メインゲームシーンを入力する
+    public string mainGameScene = "";
+
+    /// <summary>
+    /// タイトルシーンへ遷移するメソッド
+    /// </summary>
+   public void TransitionTitleScene()
     {
-        return winnerTeamName;
+        SceneManager.LoadScene(titleScene);
+    }
+
+    /// <summary>
+    /// キャラクター選択シーンへ遷移するメソッド
+    /// </summary>
+    public void TransitionChoseCharactorScene()
+    {
+        SceneManager.LoadScene(choseCharactorScene);
+    }
+
+    /// <summary>
+    /// チュートリアルシーンへ遷移するメソッド
+    /// </summary>
+    public void TransitionTutorialScene()
+    {
+        SceneManager.LoadScene(tutorialScene);
+    }
+
+    /// <summary>
+    /// メインゲームシーンへ遷移するメソッド
+    /// </summary>
+    public void TransitionMainGameScene()
+    {
+        SceneManager.LoadScene(mainGameScene);
     }
 
     public void StringWinnerName(string winnerName)
@@ -18,7 +62,7 @@ public class Singleton : MonoBehaviour
 
     private void Awake()
     {
-        if(instance == null)
+        if (instance == null)
         {
             instance = this;
 
